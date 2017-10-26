114e73f2b8ba27a912856f4c27b841ee14f3d016
Pavel Yarema <c-pyarema@zulily.com>
2017-10-25 18:01:36 -0700

    Update GO application

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,12 +10,15 @@ const version string = "2.0.1"
 
 // VersionHandler handles incoming requests to /version
 // and just returns a simple version number
-func versionHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, version)
-}
+// func versionHandler(w http.ResponseWriter, r *http.Request) {
+// 	io.WriteString(w, version)
+// }
 
 func main() {
 	log.Printf("Listening on port 8000...")
-	http.HandleFunc("/version", versionHandler)
+	//http.HandleFunc("/version", versionHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n Building GO Applications in AWS is awesome!", r.URL.Path)
+	})
 	http.ListenAndServe(":8000", nil)
 }
